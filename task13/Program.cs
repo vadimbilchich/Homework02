38736518f9fe6f04f630a18e4818cb950a587dbf
VadimBilchich <vadimbilchich@rambler.ru>
2022-10-08 13:56:11 +0800

    Решение задачи 13

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,23 @@
-﻿
-Console.WriteLine("start");
+﻿//Задача 13: Напишите метод, который выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+//32679 -> 6
+
+Console.WriteLine("task13 ");
+
+Console.Write("Enter a number: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2)
+{
+    Console.WriteLine("The third digit -> " + anyNumberText[2]);
+}
+else
+{
+    Console.WriteLine(" -> There is no third figure");
+}
+
+
