37758e918a746eb658ccddd177b1b90f6aebbf54
VadimBilchich <vadimbilchich@rambler.ru>
2022-10-08 13:40:18 +0800

    Решение задачи 10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,5 +1,13 @@
-﻿Console.WriteLine("Start");
-int x = 918;
-int y = (x % 100)/10;
+﻿//Задача 10: Напишите метод, который принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-Console.WriteLine("Вторая цифра равна - " + y);
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Clear();
+Console.WriteLine("task10" );
+Console.Write("Enter a three -digit number: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("The second digit of this number -> " + stringNumber[1]);
+
